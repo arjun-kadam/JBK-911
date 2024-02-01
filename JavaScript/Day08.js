d62d39fdfe0c2ce2ce9fb7a00a2fe09be6015391
Arjun Kadam <arjunkadama29@gmail.com>
2024-02-01 15:01:40 +0530

    Added Day 06 - Day 08

---
+++ JavaScript/Day08.js
@@ -0,0 +1,30 @@
+a=10
+a=20
+a=30
+
+console.log("Without Using Any Variable Declaration Method ==> "+a)
+
+var b=220
+b=30
+var b=400
+console.log("Using 'var' Variable Declaration Method ==> "+b)
+
+
+let c=40
+c=55
+// let c=444  // Not Possible
+ console.log("Using 'let' Variable Declaration Method ==> "+c)
+
+
+ const d=12
+ //d=377  // Not Usable & Gives Error
+//  var d=234  // Not Possible
+console.log("Using 'const' Variable Declaration Method ==> "+d)
+
+
+
+ function greet(){
+    document.write("This String is from JavaScript File");
+ }
+
+ greet()
