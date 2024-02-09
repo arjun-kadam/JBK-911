4ce11a6d95d982690c9362027e588be95f9fd8c6
Arjun Kadam <arjunkadama29@gmail.com>
2024-02-09 17:12:58 +0530

    Day 13 Adeed

---
+++ JavaScript/Day13.js
@@ -0,0 +1,23 @@
+
+let counter=0;
+function change(){
+    counter++;
+    var h1=document.getElementById("head");
+    if (h1.innerHTML=="Day 13 - Intro to DOM") {
+        h1.innerHTML="Changed Day 13"
+        h1.style.backgroundColor="red"
+        
+    } else if(h1.innerHTML=="Changed Day 13") {
+        h1.innerHTML="It's Third Time Change";
+        h1.style.backgroundColor="grey";
+        
+    }else{
+        h1.innerHTML="Day 13 - Intro to DOM";
+        h1.style.backgroundColor="yellow"
+        
+    }
+    document.getElementById("count").innerHTML="Button Clicked "+counter +" Times";
+}
+
+
+
